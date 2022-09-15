a2be2929b783e1d5dd61056deeefc6f7387eba09
khv2644511 <khv2644511@naver.com>
2022-09-16 00:30:34 +0900

    버튼 클릭 시 로딩이미지 노출됨

--- js/10000hours.js
+++ js/10000hours.js
@@ -10,3 +10,39 @@ btnOpenPopup.addEventListener('click', () => {
 btnClosePopup.addEventListener('click', () => {
   modal.style.display = 'none';
 })
+
+
+// input 값 가져오기
+// const value_field = document.getElementById('field').value;
+// const value_time = document.getElementById('time').value;
+
+
+// 나는 며칠 동안 훈련해야 1만 시간이 될까 버튼 클릭 후
+// 3초간 로딩 이미지 나타났다 사라지고,
+// 결과 화면 노출됨
+const btn_exc = document.querySelector('.btn-exc');
+const loading_img = document.querySelector('.loading_img');
+const result = document.querySelector('.cont-result');
+
+btn_exc.addEventListener('click', () => {
+  loading_img.style.display = 'flex';
+  setTimeout(() => {
+  loading_img.style.display = 'none';
+  result.style.display = 'flex';
+  }, 3000);
+})
+
+// const input_time = document.querySelector('.input_time')
+
+
+// const result2 = document.getElementById("form").onsubmit = function(){
+//   const input_field = document.querySelector('.input_field')
+//   console.log(result2.value)
+//   return false;
+
+// }
+
+
+// function calc() {
+
+// }
